ba25172a0734e54ca3378ddd8994ee6e4ac1b906
fermenem <fermenem@hotmail.com>
2023-05-30 20:38:35 -0300

    commit

--- CapaLogicaDeNegocio/GestorRespuestaOperador.cs
+++ CapaLogicaDeNegocio/GestorRespuestaOperador.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.DateTime;
+//using System.DateTime;
 
 namespace PPAI_IVR_Grupo8.CapaLogicaDeNegocio
 {
-    internal class GestorRespuestaOperador
+    public class GestorRespuestaOperador
     {
     }
 
-    public void validarOpcSeleccionada()
-    {
+    //public void validarOpcSeleccionada()
+    //{
 
-    }
+    //}
 
-    private void obtenerLlamadaActual()
-    {
-        // deberiamos llamara al metodo esEnCurso
-        // deberiamos llamara al metodo esDeTucliente
-    }
+    //private void obtenerLlamadaActual()
+    //{
+    //    // deberiamos llamara al metodo esEnCurso
+    //    // deberiamos llamara al metodo esDeTucliente
+    //}
 
-    private void marcarllamadaEnCurso()
-    {
+    //private void marcarllamadaEnCurso()
+    //{
 
-    }
+    //}
 
-    private void obtenerFechaHoraActual()
-    {
-        DateTime currentDateTime = DateTime.Now;
-    }
+    //private void obtenerFechaHoraActual()
+    //{
+    //    DateTime currentDateTime = DateTime.Now;
+    //}
 
-    private void buscarDatosLlamada()
-    {
+    //private void buscarDatosLlamada()
+    //{
 
-    }
+    //}
 }
